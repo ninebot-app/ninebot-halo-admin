31c2eaaf25c52a9bae80c55b06c389c35ed74f65
wufx <feixing.wu@ninebot.com>
2022-01-27 15:42:02 +0800

    1

--- src/config/default.js
+++ src/config/default.js
@@ -1,9 +1,9 @@
 module.exports = {
-  VIEW_PATH: 'view',
-  ADMIN_PATH: 'admin',
+  VIEW_PATH: 'v',
+  ADMIN_PATH: 'ninebotadmineditor',
   EDITOR_PATH: 'editor',
   API_PATH: 'api',
   EDITOR_TITLE: '编辑器',
-  ADMIN_TITLE: '管理后台',
+  ADMIN_TITLE: 'Ninebot-Halo管理后台',
   BAIDU_TONGJI: ''
 }
